514043a9306f92b834d58dbaf8441b42b419fddb
waltzaround <vxs4050@autuni.ac.nz>
2016-10-11 13:25:05 +1300

    delta code added

--- InteractionEngine101-master/Assets/InteractionEngine101/Scripts/RotationLogger.cs
+++ InteractionEngine101-master/Assets/InteractionEngine101/Scripts/RotationLogger.cs
@@ -5,38 +5,51 @@ using UnityEngine.UI;
 public class RotationLogger : MonoBehaviour {
 
     public GameObject textgameobject;//set this is inspector or by script, maybe GameObject.Find or something, idk.
+    private HingeJoint hinge;
+    private float prevAngle;
 
     // Use this for initialization
     void Start () {
-	
-	}
+
+
+        hinge = gameObject.GetComponent(typeof(HingeJoint)) as HingeJoint;
+        prevAngle = hinge.angle;
+
+    }
 
     // Update is called once per frame
     void Update()
     {
+        float angle = hinge.angle;
 
+        float delta = angle - prevAngle;
+        if (delta > 180) { delta -= 360; }
+        if (delta < -180) { delta += 360; }
+        Debug.Log(delta);
 
-
-        HingeJoint hinge = gameObject.GetComponent(typeof(HingeJoint)) as HingeJoint;
-
-
+        prevAngle = angle;
+/*
         Canvas canvas = gameObject.GetComponent(typeof(Canvas)) as Canvas;
 
         Text text = gameObject.GetComponent(typeof(Text)) as Text;
         hinge.angle.ToString() = text;
 
+
+
         var weightfield = GameObject.Find("CurrentValue").GetComponent("TextMesh");
         weightfield.text = totalWeight;
-
+*/
     }
 
+
+    /*
        
     void Foo()
     {
         Text text = textgameobject.GetComponent<Text>(); //get the text component in the gameobject you assigned
         text.text = "insert some text here"; //set the text in the text component
     }
-
+    */
 
 
 
